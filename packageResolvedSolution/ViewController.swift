65255ecf6149c2c68c63e84eeabb03680431b89c
Pavlo Snizhko <pavlo.snizhko@pm.bet>
2021-05-06 09:01:50 +0300

    Initial Commit

---
+++ packageResolvedSolution/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  packageResolvedSolution
+//
+//  Created by pavlo.snizhko on 06.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
